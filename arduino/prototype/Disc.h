1a998ec22f7a63a1a87a8ff0a846af7ec45b68bd
Michelle Sharer <michellesharer@gmail.com>
2022-04-03 17:13:42 -0600

    Way cleaner way to map LEDs from pin to disc

--- arduino/prototype/Disc.h
+++ arduino/prototype/Disc.h
@@ -4,9 +4,6 @@ struct Disc {
   uint8_t numInner1; // number of LEDs in inner segments of inner petal
   uint8_t numInner2; // number of LEDs in outer segments of inner petal
   uint8_t numOuter; // number of LEDs in outer segments
-  //uint8_t *pixels;
-
-  void setLED(uint8_t pixelIndex, CRGB color) {
-    setPinLED(discIndex, pixelIndex, color);
-  }
+  int offset;
+  CRGB *pixels;
 };
