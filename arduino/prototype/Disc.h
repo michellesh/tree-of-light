12435c3f4a6b6e9c90575d55cbcebe9c2bb72a37
Michelle Sharer <michellesharer@gmail.com>
2022-04-04 09:25:24 -0600

    Clean up basic rainbow code for blog post

--- arduino/prototype/Disc.h
+++ arduino/prototype/Disc.h
@@ -2,5 +2,5 @@ struct Disc {
   uint8_t discIndex;
   uint8_t numLEDs; // number of LEDs in this disc
   uint16_t offset; // number of LEDs in tree before this disc
-  CRGB *pixels;
+  CRGB *leds; // the LEDs for this disc
 };
--- arduino/prototype/prototype.ino
+++ arduino/prototype/prototype.ino
@@ -7,49 +7,44 @@
 #include "Disc.h"
 
 Disc discs[NUM_DISCS];
+CRGB *leds;
 
 void setup() {
   Serial.begin(115200);
   delay(500);
 
+  leds = new CRGB[NUM_LEDS_TOTAL];
+
+  uint16_t startIndex = 0;
+  FastLED.addLeds<NEOPIXEL, PIN_1>(leds, startIndex, NUM_LEDS_PIN_1);
+  startIndex += NUM_LEDS_PIN_1;
+  FastLED.addLeds<NEOPIXEL, PIN_2>(leds, startIndex, NUM_LEDS_PIN_2);
+  startIndex += NUM_LEDS_PIN_2;
+  FastLED.addLeds<NEOPIXEL, PIN_3>(leds, startIndex, NUM_LEDS_PIN_3);
+  startIndex += NUM_LEDS_PIN_3;
+  FastLED.addLeds<NEOPIXEL, PIN_4>(leds, startIndex, NUM_LEDS_PIN_4);
+  startIndex += NUM_LEDS_PIN_4;
+  FastLED.addLeds<NEOPIXEL, PIN_5>(leds, startIndex, NUM_LEDS_PIN_5);
+  startIndex += NUM_LEDS_PIN_5;
+  FastLED.addLeds<NEOPIXEL, PIN_6>(leds, startIndex, NUM_LEDS_PIN_6);
+  startIndex += NUM_LEDS_PIN_6;
+  FastLED.addLeds<NEOPIXEL, PIN_7>(leds, startIndex, NUM_LEDS_PIN_7);
+
   uint16_t offset = 0;
   for (uint8_t d = 0; d < NUM_DISCS; d++) {
-    Disc disc = {d, NUM_LEDS[d], offset};
+    Disc disc = {d, NUM_LEDS_DISC[d], offset, &leds[offset]};
     discs[d] = disc;
     offset += disc.numLEDs;
-  }
-
-  leds = new CRGB[NUM_LEDS_PIN_1 +
-                  NUM_LEDS_PIN_2 +
-                  NUM_LEDS_PIN_3 +
-                  NUM_LEDS_PIN_4 +
-                  NUM_LEDS_PIN_5 +
-                  NUM_LEDS_PIN_6 +
-                  NUM_LEDS_PIN_7];
-
-  uint16_t totalLEDs = 0;
-  FastLED.addLeds<LED_TYPE, PIN_1>(leds, totalLEDs, NUM_LEDS_PIN_1);
-  totalLEDs += NUM_LEDS_PIN_1;
-  FastLED.addLeds<LED_TYPE, PIN_2>(leds, totalLEDs, NUM_LEDS_PIN_2);
-  totalLEDs += NUM_LEDS_PIN_2;
-  FastLED.addLeds<LED_TYPE, PIN_3>(leds, totalLEDs, NUM_LEDS_PIN_3);
-  totalLEDs += NUM_LEDS_PIN_3;
-  FastLED.addLeds<LED_TYPE, PIN_4>(leds, totalLEDs, NUM_LEDS_PIN_4);
-  totalLEDs += NUM_LEDS_PIN_4;
-  FastLED.addLeds<LED_TYPE, PIN_5>(leds, totalLEDs, NUM_LEDS_PIN_5);
-  totalLEDs += NUM_LEDS_PIN_5;
-  FastLED.addLeds<LED_TYPE, PIN_6>(leds, totalLEDs, NUM_LEDS_PIN_6);
-  totalLEDs += NUM_LEDS_PIN_6;
-  FastLED.addLeds<LED_TYPE, PIN_7>(leds, totalLEDs, NUM_LEDS_PIN_7);
-
-  for (uint8_t d = 0; d < NUM_DISCS; d++) {
-    discs[d].pixels = &leds[discs[d].offset];
   }
 
   FastLED.setBrightness(50);
 }
 
 void loop() {
-  ripple();
+  for (uint8_t d = 0; d < NUM_DISCS; d++) {
+    for (uint8_t p = 0; p < discs[d].numLEDs; p++) {
+      discs[d].leds[p] = rainbow[d];
+    }
+  }
   FastLED.show();
 }
