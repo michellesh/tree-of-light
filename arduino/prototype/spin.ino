c090f2773840d9835e00050945c3aae9ebaad1e2
Michelle Sharer <michellesharer@gmail.com>
2022-04-05 20:42:15 -0600

    Refactor spin code a bit

--- arduino/prototype/spin.ino
+++ arduino/prototype/spin.ino
@@ -5,29 +5,20 @@ float percentRadius = 0.8;
 void spin() {
   for (uint8_t d = 0; d < NUM_DISCS; d++) {
     for (uint8_t p = 0; p < discs[d].numLEDs; p++) {
+      // only light up LEDs close to the edge of the disc
       if (discs[d].radius(p) < percentRadius * discs[d].maxRadius) {
         discs[d].leds[p] = CRGB::Black;
         continue;
       }
 
       int16_t ledAngle = discs[d].angle(p);
-      int16_t dist = getDist(angle, ledAngle);
-      bool isInRange = dist < maxWidth;
-      if (!isInRange && angle < maxWidth) {
-        dist = getDist(360 + angle, ledAngle);
-        isInRange = dist < maxWidth;
+      if (setLED(d, p, angle, ledAngle) ||
+         (angle < maxWidth && setLED(d, p, 360 + angle, ledAngle)) ||
+         (angle > 360 - maxWidth && setLED(d, p, angle - 360, ledAngle))) {
+        continue;
       }
-      if (!isInRange && angle > 360 - maxWidth) {
-        dist = getDist(angle - 360, ledAngle);
-        isInRange = dist < maxWidth;
-      }
-      if (isInRange) {
-        uint8_t fade = map(dist, 0, maxWidth, 0, 255);
-        discs[d].leds[p] = campfire[d];
-        discs[d].leds[p].fadeLightBy(fade);
-      } else {
-        discs[d].leds[p] = CRGB::Black;
-      }
+
+      discs[d].leds[p] = CRGB::Black;
     }
   }
 
@@ -35,6 +26,24 @@ void spin() {
   if (angle >= 360) {
     angle = 0;
   }
+}
+
+bool setLED(uint8_t d, uint8_t p, int16_t angle, int16_t ledAngle) {
+  int16_t dist = getDist(angle, ledAngle);
+  if (inRange(dist)) {
+    setColor(d, p, dist);
+    return true;
+  }
+  return false;
+}
+
+void setColor(uint8_t d, uint8_t p, int16_t dist) {
+  discs[d].leds[p] = campfire[d];
+  discs[d].leds[p].fadeLightBy(map(dist, 0, maxWidth, 0, 255));
+}
+
+bool inRange(int16_t dist) {
+  return dist < maxWidth;
 }
 
 int16_t getDist(int16_t angle, int16_t ledAngle) {
